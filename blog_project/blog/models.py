c2053928a8d8db6416223a60feaba3496edc6c35
Mohamamd AlMarzouq <mo.almarzouq@ku.edu.kw>
2025-03-12 10:45:59 +0300

    admin and models class

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title} created on {self.created_on}"
